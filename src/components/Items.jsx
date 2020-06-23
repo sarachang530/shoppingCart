9a000cce4d7e50e335b6e5853ec22fc21255e82d
Sara Chang <sarachang530@gmail.com>
2020-06-23 17:59:52 -0400

    created api fetch reqs

--- src/components/Items.jsx
+++ src/components/Items.jsx
@@ -1,11 +1,11 @@
 import React, { useState, useEffect } from 'react';
 
-
-const fetchURL = 'https://gopuff-public.s3.amazonaws.com/dev-assignments/product/order.json?fbclid=IwAR3DsqgJIiYhFmp4Rksyg8WMyglNaiBzU7ZUK1evtw3PFrXiGXIBQfygiEw';
+const fetchURL =
+  'https://gopuff-public.s3.amazonaws.com/dev-assignments/product/order.json?fbclid=IwAR3DsqgJIiYhFmp4Rksyg8WMyglNaiBzU7ZUK1evtw3PFrXiGXIBQfygiEw';
 const getItems = () => fetch(fetchURL).then(res => res.json());
 
 const Items = () => {
-  const [items, setItems] = useState();
+  const [items, setItems] = useState([]);
 
   useEffect(() => {
     getItems().then(data => setItems(data));
@@ -13,16 +13,14 @@ const Items = () => {
 
   return (
     <div>
+      {console.log(items)}
       <ul>
-        {items
-        ? items.map(item => {
-            return <div key={item.id}>{item.title}</div>;
-          })
-        : "Loading..."}
+        {items.map(item => {
+          <div key={item.id}>{item.title}</div>;
+        })}
       </ul>
     </div>
   );
 };
 
 export default Items;
-
