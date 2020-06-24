5fa9bdf11f6942e83f700256cbc62327ebaa9fc7
Sara Chang <sarachang530@gmail.com>
2020-06-24 10:03:19 -0400

    created comments for server.js

--- server/server.js
+++ server/server.js
@@ -4,14 +4,19 @@ const cookieParser = require('cookie-parser')
 
 const app = express();
 
+/* Require Route Handlers */
+
+/* Define PORT */
 const PORT = process.env.PORT || 3000;
 
-/* Express logic/handler */
+/* Enable Express handlers */
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
+
+/* Use cookieParser */
 app.use(cookieParser());
 
-
+/* Serves Static Files */
 app.use(express.static(path.resolve(__dirname, '../dist')));
 
 app.use(express.static(path.join(__dirname, '../src')));
@@ -19,17 +24,19 @@ app.get('/', (req, res) => {
   res.sendFile(path.join(__dirname, '../src/index.html'));
 });
 
+/* 404 Error Handler */
 app.use('*', (req, res) => res.sendStatus(404));
 
+/* Global Error Handling */
 app.use((err, req, res, next) => {
   const defaultErr = {
     log: 'Express error handler caught unknown middleware error',
     status: 500,
     message: { err: 'An error occurred' },
   };
-  const errObj = { ...defaultErr, ...err };
-  console.log(errObj.log);
-  res.status(errObj.status).json(errObj.message);
+  const newErrObj = { ...defaultErr, ...err };
+  console.log(newErrObj.log);
+  res.status(newErrObj.status).json(newErrObj.message);
 });
 
 app.listen(PORT, () => console.log(`Listening on port ${PORT}`));
